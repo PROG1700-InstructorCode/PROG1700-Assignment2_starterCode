471e9ccaab044fb21e9334fbc64308f306834cc4
Hamlet Lin <hamlet.lin@nscc.ca>
2022-10-05 11:45:24 -0300

    added indivisual file and launch.json

---
+++ p1-landscape.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+Student Name:   [Enter name here]
+Date:           [Enter date here]  
+Program Title:  
+Description:    
+*/
+
+function main() // <-- Don't change this line!
+{   
+
+
+}
+
+// Do not change any of the code below!
+if (require.main === module)
+{
+    main();
+}
